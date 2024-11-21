dfd3ffdc348b8700dc4e98e744a28ec6ce87f53d
Asad-wego <asad@wego.com>
2024-11-21 10:02:38 +0500

    Debounce Utility
    Add a debounce function to optimize API call handling.

---
+++ src/utils/debounce.ts
@@ -0,0 +1,14 @@
+/*
+ * Created by Asad on 21 Nov 2024
+ */
+
+export const debounce = <T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+): ((...args: Parameters<T>) => void) => {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return (...args: Parameters<T>) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+};
